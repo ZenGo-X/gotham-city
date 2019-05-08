a7ed5996d3d8138935516dfb820fa46db1d65865
Oded Leiba <oded@kzencorp.com>
2019-05-08 12:06:21 +0300

    fix integration tests

--- integration-tests/src/test.rs
+++ integration-tests/src/test.rs
@@ -6,7 +6,6 @@ mod tests {
     extern crate kms;
 
     use client_lib::api::{PrivateShare, ClientShim};
-    use bitcoin::util::hash::Sha256dHash;
     use curv::arithmetic::traits::Converter;
     use curv::BigInt;
     use server_lib::server;
@@ -19,21 +18,19 @@ mod tests {
             server::get_server().launch();
         });
 
-        let client_shim = ClientShim::new("http://localhost:8000".to_string());
+        let client_shim = ClientShim::new("http://localhost:8000".to_string(), None);
 
         let five_seconds = time::Duration::from_millis(5000);
         thread::sleep(five_seconds);
 
         let ps: PrivateShare = client_lib::api::get_master_key(&client_shim);
 
-        let pos = 0;
         let child_master_key = ps
             .master_key
-            .get_child(vec![BigInt::from(pos)]);
+            .get_child(vec![BigInt::from(0)]);
 
-        let data : &[u8] = &[];
-        let hash : Sha256dHash = Sha256dHash::from_data(data);
-        let signature = client_lib::api::sign(&client_shim, hash, &child_master_key, pos, &ps.id);
+        let msg: BigInt = BigInt::from(1234);  // arbitrary message
+        let signature = client_lib::api::sign(&client_shim, msg, &child_master_key, BigInt::from(0), BigInt::from(0), &ps.id);
         println!(
             "signature = (r: {}, s: {})",
             signature.r.to_hex(),
