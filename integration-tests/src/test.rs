d7faac98ef4add17094770f4bca881b3ef70347a
Matan Hamilis <matan.hamilis@gmail.com>
2022-07-19 11:04:53 +0300

    next step in rebase

--- integration-tests/src/test.rs
+++ integration-tests/src/test.rs
@@ -2,62 +2,87 @@
 mod tests {
     extern crate client_lib;
     extern crate server_lib;
+    extern crate two_party_ecdsa;
+    extern crate secp256k1;
+    extern crate ed25519_dalek;
 
     use client_lib::*;
     use rocket::Config;
     use server_lib::server;
     use std::collections::HashMap;
     use std::{thread, time};
+    use two_party_ecdsa::curv::elliptic::curves::traits::ECPoint;
+    use two_party_ecdsa::curv::arithmetic::traits::{Samplable, Converter};
 
     #[rocket::async_test]
     async fn test_ecdsa() {
-        rocket::tokio::spawn(spawn_server(8000, "ecdsa"));
+        use secp256k1::{Secp256k1, Signature, Message};
 
-        let five_seconds = time::Duration::from_millis(5000);
-        thread::sleep(five_seconds);
+        rocket::tokio::spawn(spawn_server(8000, "ecdsa"));
 
         let client_shim = ClientShim::new("http://localhost:8000".to_string(), None);
 
-        let ps: ecdsa::PrivateShare = ecdsa::get_master_key(&client_shim);
+        let two_seconds = time::Duration::from_millis(2000);
+        thread::sleep(two_seconds);
 
-        for y in 0..10 {
-            let x_pos = BigInt::from(0);
+        let ps: ecdsa::PrivateShare = ecdsa::get_master_key(&client_shim);
+        let ctx = Secp256k1::verification_only();
+
+        for y in 0..10i32 {
+            let x_pos = BigInt::from(y*2+1);
             let y_pos = BigInt::from(y);
             println!("Deriving child_master_key at [x: {}, y:{}]", x_pos, y_pos);
 
             let child_master_key = ps.master_key.get_child(vec![x_pos.clone(), y_pos.clone()]);
+            let pk = child_master_key.public.q.get_element();
 
-            let msg: BigInt = BigInt::from(y + 1); // arbitrary message
-            let signature = ecdsa::sign(&client_shim, msg, &child_master_key, x_pos, y_pos, &ps.id)
+
+            let msg: BigInt = BigInt::sample(256); // arbitrary message
+            let signature = ecdsa::sign(&client_shim, msg.clone(), &child_master_key, x_pos, y_pos, &ps.id)
                 .expect("ECDSA signature failed");
 
-            println!(
-                "signature = (r: {}, s: {})",
-                signature.r.to_hex(),
-                signature.s.to_hex()
-            );
+            let msg_int = BigInt::to_vec(&msg);
+            let r = BigInt::to_vec(&signature.r);
+            let s = BigInt::to_vec(&signature.s);
+            let mut msg = [0u8; 32];
+            msg[32-msg_int.len()..].copy_from_slice(&msg_int);
+            let msg = Message::from_slice(&msg).unwrap();
+
+            let mut sig = [0u8; 64];
+            sig[32-r.len()..32].copy_from_slice(&r);
+            sig[32+32-s.len()..].copy_from_slice(&s);
+
+            let sig = Signature::from_compact(&sig).unwrap();
+
+            ctx.verify(&msg, &sig, &pk).unwrap();
         }
     }
 
     #[rocket::async_test]
     async fn test_eddsa() {
+        use rand::Rng;
+        use ed25519_dalek::{PublicKey, Signature, Verifier};
         rocket::tokio::spawn(spawn_server(8002, "eddsa"));
 
         let client_shim = ClientShim::new("http://localhost:8002".to_string(), None);
 
-        let five_seconds = time::Duration::from_millis(5000);
-        thread::sleep(five_seconds);
+        let two_seconds = time::Duration::from_millis(2000);
+        thread::sleep(two_seconds);
 
         let (key_pair, key_agg, id) = client_lib::eddsa::generate_key(&client_shim).unwrap();
 
-        let message = [74u8, 24u8, 37u8, 20u8, 12u8, 3u8, 14u8];
-        let signature = client_lib::eddsa::sign(&client_shim, &message, &key_pair, &key_agg, &id)
+        let mut rng = rand::thread_rng();
+        let mut msg = [0u8; 32];
+        for _ in 0..2_000 {
+            let msg_len = rng.gen::<u8>() as usize / 8;
+            let msg = &mut msg[0..msg_len];
+            rng.fill(msg);
+            let signature = client_lib::eddsa::sign(&client_shim, msg, &key_pair, &key_agg, &id)
             .expect("EdDSA signature failed");
-
-        println!(
-            "signature = {:?}",
-            signature
-        );
+            let pubkey = PublicKey::from_bytes(&key_agg.aggregated_pubkey()).unwrap();
+            let sig = Signature::from_bytes(&signature.serialize()).unwrap();
+            pubkey.verify(msg, &sig).unwrap();
+        }
     }
 
     async fn spawn_server(port: u32, db_name: &str) {
