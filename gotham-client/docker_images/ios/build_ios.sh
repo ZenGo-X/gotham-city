8049740d8e62f1a7f33b968f822f957f7012ade3
Iraklis Leontiadis <iraklis@zengo.com>
2023-05-15 13:24:28 +0200

    remove x86 from final lib

--- gotham-client/docker_images/ios/build_ios.sh
+++ gotham-client/docker_images/ios/build_ios.sh
@@ -19,6 +19,6 @@ cargo +${IOS_TOOLCHAIN} build --target aarch64-apple-ios --release --lib
 ## BUILD INTEL
 cargo build --target x86_64-apple-ios --release --lib
 
-lipo -create -output libclient_lib.a ../../../target/{aarch64-apple-ios,x86_64-apple-ios}/release/libclient_lib.a 
+lipo -create -output libclient_lib.a ../../../target/{aarch64-apple-ios}/release/libclient_lib.a
 
 popd .
