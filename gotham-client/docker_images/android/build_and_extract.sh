e5c678e2127a256a65123621657ffa7777819ca2
Iraklis Leontiadis <iraklis@zengo.com>
2023-08-02 13:52:11 +0300

    init

--- gotham-client/docker_images/android/build_and_extract.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker build -t gotham .
-id=$(docker create gotham)
-docker cp $id:/gotham-city/gotham-client/jniLibs_debug .
-docker cp $id:/gotham-city/gotham-client/jniLibs .
-docker rm -v $id
